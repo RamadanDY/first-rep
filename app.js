11df73d1df9214d257d82e615e24a22053136168
RamadanDY <daudalaramadan@gmail.com>
2023-12-18 23:56:39 +0000

    pro

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('name');
+const close = document.querySelector(".popup-close");
+const button = document.getElementById("btn");
+const wrapper = document.querySelector(".popup-wrapper");
+
+
+
+
+
+
+button.addEventListener("click", () => {
+  wrapper.style.display = "block";
+  console.log("name");
+});
